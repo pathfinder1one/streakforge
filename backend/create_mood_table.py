38a7d1c6a9d8a07d091602a0903cb0f6efd82b03
pathfinder1one <chiragtejasvi@gmail.com>
2026-06-30 01:46:46 +0530

    chore: remove unused temporary database migration scripts

--- backend/create_mood_table.py
+++
@@ -1,6 +0,0 @@
-from app.core.database import engine, Base
-import app.models.user
-
-# Create table if it doesn't exist
-Base.metadata.create_all(bind=engine, tables=[app.models.user.MoodLog.__table__])
-print("MoodLog table created.")
